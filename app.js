fc210c6625d291cd79876a7a1e01538a6ca0299e
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-03 00:21:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
@@ -25,6 +25,7 @@ app.use(function(req, res, next) {
 
 // const user = require('./Controllers/User.controller');
 app.use('/api/auth', require('./Controllers/auth.controller'));
+app.use('/api/profile', require('./Controllers/profile.controller'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
