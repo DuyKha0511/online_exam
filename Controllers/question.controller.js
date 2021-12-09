149c2272140104c1fbcb0f0da54c1c66afc03d4e
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-09 12:51:09 +0700

    Update question.controller.js

--- Controllers/question.controller.js
+++ Controllers/question.controller.js
@@ -5,17 +5,6 @@ const jwt = require('jsonwebtoken');
 const status = require('../Config/status.json');
 const middleware = require('../_Middleware/question.middleware');
 
-// function verifyToken(req, res, next) {
-//     const authorizationHeader = req.headers['authorization'];
-//     // 'Bear [token]'
-//     const token = authorizationHeader.split(' ')[1];
-//     if (!token) res.json({status: status.Unauthorized});
-
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
-//         if (err) res.json({status: status.Forbidden});
-//         next();
-//     })
-// }
 
 router.get('/',  middleware.verifyToken,  middleware.checkRole_View, (req, res) => {
     console.log('api/questions called!!!!');
@@ -49,5 +38,12 @@ router.get('/user/:userID', middleware.verifyToken, middleware.checkRole_View, (
     });
 })
 
+router.get('/lib/:LibraryFolderID', middleware.verifyToken, middleware.checkRole_View, (req, res) => {
+    var LibraryFolderID = req.params.LibraryFolderID;
+    console.log(`api/questions/lib/${LibraryFolderID} called!!!!`);
+    questionHandle.getQuestionsByLibraryFolder(LibraryFolderID).then(function(value) {
+        res.json({status: status.Access, data: value.recordsets[0]});
+    });
+})
 
 module.exports = router;
