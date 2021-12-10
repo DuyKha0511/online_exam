89b531d8693a750497cd1328d3c9957163bff6fc
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-11 00:53:55 +0700

    Update class.middleware.js

--- _Middleware/class.middleware.js
+++ _Middleware/class.middleware.js
@@ -9,10 +9,10 @@ function verifyToken(req, res, next) {
     const authorizationHeader = req.headers['authorization'];
     // 'Bear [token]'
     const token = authorizationHeader.split(' ')[1];
-    if (!token) res.json({status: status.Unauthorized, message: 'Unauthorized'});
+    if (!token) res.json({status: status.Error, message: 'Error Token'});
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
-        if (err) res.json({status: status.Forbidden, message: 'Error Token'});
+        if (err) res.json({status: status.Unauthorized, message: 'Unauthorized'});
         try {
             req.Username = data.Username;
             req.UserID = data.UserID;
