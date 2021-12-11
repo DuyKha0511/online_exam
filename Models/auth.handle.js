611a55c20cedce49ac74cdec5e6e4e50587bda1a
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-12 02:23:50 +0700

    Update auth.handle.js

--- Models/auth.handle.js
+++ Models/auth.handle.js
@@ -25,19 +25,12 @@ module.exports = {
         `);
     },
     signup: function(username, password, email) {
-//         ExcuteSQL(`
-//             insert into tb_RoleOfUser values
-//             ((select UserID from tb_User where Username = '${username}'), 3)
-//         `);
-        sql.connect(db_config)
-        .then(function() {
-            sql.Request().query(`
-                insert into tb_RoleOfUser values
-                ((select UserID from tb_User where Username = '${username}'), 3)
-            `)});
         return ExcuteSQL(`
             insert into tb_User (Username, Password, Email, Authentication) 
             values ('${username}', '${password}', '${email}', 0)
+            and
+            insert into tb_RoleOfUser values
+            ((select UserID from tb_User where Username = '${username}'), 3)
         `);
     },
     verify: function(username) {
