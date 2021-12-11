af851ca3fe1fac092199978c8a5f634144f1b8c2
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-11 11:05:20 +0700

    Update profile.middleware.js

--- _Middleware/profile.middleware.js
+++ _Middleware/profile.middleware.js
@@ -5,8 +5,9 @@ const jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
     const authorizationHeader = req.headers['authorization'];
     // 'Bear [token]'
-    if (!token) res.json({status: status.Error, message: 'Error Token'});
+    if (!authorizationHeader) res.json({status: status.Error, message: 'Error Header Authorization'});
     const token = authorizationHeader.split(' ')[1];
+    if (!token) res.json({status: status.Error, message: 'Error Token'});
     
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
         if (err) res.json({status: status.Unauthorized, message: 'Unauthorized'});
