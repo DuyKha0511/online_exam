953bffda1d4e174adf98da57e9217df5d6901379
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-13 22:54:03 +0700

    Update profile.controller.js

--- Controllers/profile.controller.js
+++ Controllers/profile.controller.js
@@ -5,33 +5,6 @@ const jwt = require('jsonwebtoken');
 const status = require('../Config/status.json');
 const middleware = require('../_Middleware/profile.middleware');
 
-/**
- * @swagger
- * /api/profile:
- *  get:
- *    tags: 
- *    - "Auth Server"
- *    summary: "Self Profile"
- *    description: Self Profile of an account
- *    consumes:
- *    - "application/json"
- *    produces:
- *    - "application/json"
- *    parameters:
- *    - in: "headers"
- *      name: "authorization"
- *      description: "Json Web Token"
- *      required: true
- *    security:
- *    - Bearer: []
- *    responses:
- *      '200':
- *        description: "status: Access | data"
- *      '601':
- *        description: "status: Forbidden/Access Denied | message: Error Token"
- *      '602':
- *        description: "status: Unauthorized | message: Unauthorized"
- */
 router.get('/', middleware.verifyToken, (req, res) => {
     console.log('api/profile called!!!!');
     profileHandle.profile(req.Username).then(function(value) {
@@ -46,5 +19,17 @@ router.post('/update', middleware.verifyToken, (req, res) => {
     });
 })
 
+router.post('/password', middleware.verifyToken, (req, res) => {
+    console.log('api/profile/password called!!!!');
+    var OldPassword = req.body.OldPassword;
+    var NewPassword = req.body.NewPassword;
+    profileHandle.checkPassword(req.UserID, OldPassword).then(function(result) {
+        if (result.recordset.length == 1) 
+            profileHandle.changePassword(req.UserID, OldPassword, NewPassword).then(function(value) {
+                res.json({status: status.Access})
+            });
+        else res.json({status: status.Error, message: 'Password is incorrect!'})
+    }) 
+})
 
 module.exports = router;
