a3261c8d47104d90733e3739b56b87333c9b5272
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-13 23:50:33 +0700

    Update class.middleware.js

--- _Middleware/class.middleware.js
+++ _Middleware/class.middleware.js
@@ -6,20 +6,25 @@ const class_GroupFunction = 7
 
 function verifyToken(req, res, next) {
     const authorizationHeader = req.headers['authorization'];
-    // 'Bear [token]'
-    if (!authorizationHeader) res.json({status: status.Error, message: 'Error Header Authorization'});
-    const token = authorizationHeader.split(' ')[1];
-    if (!token) res.json({status: status.Error, message: 'Error Token'});
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
-        if (err) res.json({status: status.Unauthorized, message: 'Unauthorized'});
-        try {
-            req.Username = data.Username;
-            req.UserID = data.UserID;
-            next();
+    if (!authorizationHeader) {
+        console.log('Error Header Authorization');
+        res.json({status: status.Error, message: 'Error Header Authorization'});
+    }
+    else {
+        token = authorizationHeader.split(' ')[1];
+        if (!token) res.json({status: status.Error, message: 'Error Token'});
+        else {
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+                if (err) res.json({status: status.Unauthorized, message: 'Unauthorized'});
+                try { 
+                    req.Username = data.Username;
+                    req.UserID = data.UserID;
+                    next();
+                }
+                catch {}
+            })
         }
-        catch {}
-    })
+    };
 }
 
 function checkRole_View(req, res, next) {
