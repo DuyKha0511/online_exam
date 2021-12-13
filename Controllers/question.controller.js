3165d546bcfca5a2013dc719da0758c98f345c5a
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-14 02:31:45 +0700

    Update question.controller.js

--- Controllers/question.controller.js
+++ Controllers/question.controller.js
@@ -73,50 +73,53 @@ router.get('/lib/:LibraryFolderID', middleware.verifyToken, middleware.checkRole
     var LibraryFolderID = req.params.LibraryFolderID;
     console.log(`api/questions/lib/${LibraryFolderID} called!!!!`);
     questionHandle.getQuestionsByLibraryFolder(LibraryFolderID).then(function(value) {
-        var id = value.recordset[0].QuestionID;
-        var data = []
-        var question = {
-            QuestionID: value.recordset[0].QuestionID, 
-            Question: value.recordset[0].Question, 
-            Type: value.recordset[0].Type,
-            Level: value.recordset[0].Level, 
-            LibraryFolderID: value.recordset[0].LibraryFolderID,
-        }
-        var Solution = [{
-            SolutionID: value.recordset[0].SolutionID, 
-            Solution: value.recordset[0].Solution, 
-            Correct: value.recordset[0].Correct
-        }]
-        for (let i = 1; i < value.recordset.length; i++) {
-            if (value.recordset[i].QuestionID === id) {
-                Solution.push({
-                    SolutionID: value.recordset[i].SolutionID, 
-                    Solution: value.recordset[i].Solution, 
-                    Correct: value.recordset[i].Correct
-                })
+        if (value.recordset.length > 0) {
+            var id = value.recordset[0].QuestionID;
+            var data = []
+            var question = {
+                QuestionID: value.recordset[0].QuestionID, 
+                Question: value.recordset[0].Question, 
+                Type: value.recordset[0].Type,
+                Level: value.recordset[0].Level, 
+                LibraryFolderID: value.recordset[0].LibraryFolderID,
             }
-            else {
-                question.Solution = Solution;
-                data.push(question);
-                id = value.recordset[i].QuestionID;
-                Solution = []
-                Solution.push({
-                    SolutionID: value.recordset[i].SolutionID, 
-                    Solution: value.recordset[i].Solution, 
-                    Correct: value.recordset[i].Correct
-                })
-                question = {
-                    QuestionID: value.recordset[i].QuestionID, 
-                    Question: value.recordset[i].Question, 
-                    Type: value.recordset[i].Type,
-                    Level: value.recordset[i].Level, 
-                    LibraryFolderID: value.recordset[i].LibraryFolderID,
+            var Solution = [{
+                SolutionID: value.recordset[0].SolutionID, 
+                Solution: value.recordset[0].Solution, 
+                Correct: value.recordset[0].Correct
+            }]
+            for (let i = 1; i < value.recordset.length; i++) {
+                if (value.recordset[i].QuestionID === id) {
+                    Solution.push({
+                        SolutionID: value.recordset[i].SolutionID, 
+                        Solution: value.recordset[i].Solution, 
+                        Correct: value.recordset[i].Correct
+                    })
+                }
+                else {
+                    question.Solution = Solution;
+                    data.push(question);
+                    id = value.recordset[i].QuestionID;
+                    Solution = []
+                    Solution.push({
+                        SolutionID: value.recordset[i].SolutionID, 
+                        Solution: value.recordset[i].Solution, 
+                        Correct: value.recordset[i].Correct
+                    })
+                    question = {
+                        QuestionID: value.recordset[i].QuestionID, 
+                        Question: value.recordset[i].Question, 
+                        Type: value.recordset[i].Type,
+                        Level: value.recordset[i].Level, 
+                        LibraryFolderID: value.recordset[i].LibraryFolderID,
+                    }
                 }
             }
+            question.Solution = Solution;
+            data.push(question);
+            res.json({status: status.Access, data: data});
         }
-        question.Solution = Solution;
-        data.push(question);
-        res.json({status: status.Access, data: data});
+        else res.json({status: status.Access, data: []});
     });
 })
 
