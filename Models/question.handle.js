fc9f72e26119c17362436715287904c13269476f
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-14 15:30:35 +0700

    Update question.handle.js

--- Models/question.handle.js
+++ Models/question.handle.js
@@ -35,5 +35,43 @@ module.exports = {
     },
     getSolution: function(questionID) {
         return ExcuteSQL(`select * from tb_Solution where QuestionID = ${questionID}`);
+    },
+    createQuestion: function(QuestionData, LibraryFolderID) {
+        var query = `INSERT INTO tb_Question VALUES (N'${QuestionData.Question}', '${QuestionData.Type}', '${QuestionData.Level}', ${LibraryFolderID})\n`
+        +   `DECLARE @QuestionID INT\n` 
+        +   `SELECT @QuestionID = QuestionID FROM tb_Question WHERE\n` 
+        +   `Question = N'${QuestionData.Question}' AND \n`
+        +   `Type = '${QuestionData.Type}' AND \n`
+        +   `Level = '${QuestionData.Level}' AND LibraryFolderID = ${LibraryFolderID}\n`
+        +   `INSERT INTO tb_Solution VALUES \n`;
+        QuestionData.Solution.map((value, index) => {
+            if (index < QuestionData.Solution.length - 1)
+                query += `(N'${value.Solution}', ${value.Correct ? 1 : 0}, @QuestionID),\n`
+            else  query += `(N'${value.Solution}', ${value.Correct ? 1 : 0}, @QuestionID)`
+        });
+        query += `SELECT QuestionID FROM tb_Question WHERE\n`
+        +   `Question = N'${QuestionData.Question}' AND\n`
+        +   `Type = '${QuestionData.Type}' AND \n`
+        +   `Level = '${QuestionData.Level}' AND \n`
+        +   `LibraryFolderID = ${LibraryFolderID}`
+        return ExcuteSQL(query);
+    },
+    checkQuestion: function(QuestionData, LibraryFolderID) {
+        return ExcuteSQL(`
+            SELECT * FROM tb_Question WHERE 
+            Question = N'${QuestionData.Question}' AND 
+            Type = '${QuestionData.Type}' AND 
+            Level = '${QuestionData.Level}' AND 
+            LibraryFolderID = ${LibraryFolderID}
+        `);
+    },
+    deleteQuestion: function(questionID) {
+        var query = `(`;
+        questionID.map((value, index) => {
+            if (index < questionID.length - 1)
+                query += `${value}, `
+            else  query += `${value})`
+        });
+        return ExcuteSQL(`DELETE FROM tb_Question WHERE QuestionID IN ${query}`);
     }
 }
