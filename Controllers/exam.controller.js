833c677688ffad7f013aa8a0a8512a63759921c3
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-16 02:48:36 +0700

    Update exam.controller.js

--- Controllers/exam.controller.js
+++ Controllers/exam.controller.js
@@ -92,16 +92,96 @@ router.put('', middleware.verifyToken, middleware.checkRole_Create, (req, res) =
 
 router.delete('/:ExamID', middleware.verifyToken, middleware.checkRole_Update, (req, res) => {
     console.log(`api/exams/${req.params.ExamID} delete exam called!!!`);
-    examHandle.deleteExam(req.params.ExamID).then(() => {
-        res.json({status: status.Access});
-    })
+    const ClassID = req.body.ClassID;
+    examHandle.getExamByID(req.params.ExamID).then((results) => {
+        tspBegin = new Date(results.recordset[0].TimeBegin).getTime();
+        tspEnd = new Date(results.recordset[0].TimeEnd).getTime();
+        tspNow = new Date().getTime() + 7*3600000;
+        if (tspNow < tspBegin) {
+            examHandle.deleteExamInClass(req.params.ExamID, ClassID).then(() => {
+                res.json({status: status.Access});
+            });
+        }
+        else if (tspNow >= tspBegin && tspNow <= tspEnd)
+            res.json({status: status.Error, message: "The exam is in progress!"})
+        else if (tspNow > tspEnd)
+            res.json({status: status.Error, message: "The exam had been done!"})
+    });
 });
 
 router.post('/:ExamID', middleware.verifyToken, middleware.checkRole_Update, (req, res) => {
     console.log(`api/exams/${req.params.ExamID} update exam called!!!`);
-    examHandle.updateExam(req.params.ExamID, req.body).then(() => {
-        res.json({status: status.Access});
-    })
+    examHandle.getExamByID(req.params.ExamID).then((results) => {
+        tspBegin = new Date(results.recordset[0].TimeBegin).getTime();
+        tspEnd = new Date(results.recordset[0].TimeEnd).getTime();
+        tspNow = new Date().getTime() + 7*3600000;
+        if (tspNow < tspBegin) {
+            examHandle.updateExam(req.params.ExamID, req.body).then(() => {
+                res.json({status: status.Access});
+            })
+        }        
+        else if (tspNow >= tspBegin && tspNow <= tspEnd)
+            res.json({status: status.Error, message: "The exam is in progress!"})
+        else if (tspNow > tspEnd)
+            res.json({status: status.Error, message: "The exam had been done!"})
+    });
+});
+
+router.get('/do-exam/:ExamID', middleware.verifyToken, middleware.checkRole_ViewInfo, (req, res) => {
+    const ExamID = req.params.ExamID;
+    console.log(`api/exams/${ExamID} do-exam  called!!!!`);
+    examHandle.getExamByID(ExamID).then((exam) => {
+        var data = exam.recordset[0];
+        examHandle.getQuestionsOfExam(ExamID).then((questions) => {
+            var listQuestions = []
+            if (questions.recordset.length > 0) {
+                var id = questions.recordset[0].QuestionID;
+                var question = {
+                    QuestionID: questions.recordset[0].QuestionID, 
+                    Question: questions.recordset[0].Question, 
+                    Type: questions.recordset[0].Type,
+                    Level: questions.recordset[0].Level, 
+                    LibraryFolderID: questions.recordset[0].LibraryFolderID,
+                }
+                var Solution = [{
+                    SolutionID: questions.recordset[0].SolutionID, 
+                    Solution: questions.recordset[0].Solution, 
+                    Correct: null
+                }]
+                for (let i = 1; i < questions.recordset.length; i++) {
+                    if (questions.recordset[i].QuestionID === id) {
+                        Solution.push({
+                            SolutionID: questions.recordset[i].SolutionID, 
+                            Solution: questions.recordset[i].Solution, 
+                            Correct: null
+                        })
+                    }
+                    else {
+                        question.Solution = Solution;
+                        listQuestions.push(question);
+                        id = questions.recordset[i].QuestionID;
+                        Solution = []
+                        Solution.push({
+                            SolutionID: questions.recordset[i].SolutionID, 
+                            Solution: questions.recordset[i].Solution, 
+                            Correct: null
+                        })
+                        question = {
+                            QuestionID: questions.recordset[i].QuestionID, 
+                            Question: questions.recordset[i].Question, 
+                            Type: questions.recordset[i].Type,
+                            Level: questions.recordset[i].Level, 
+                            LibraryFolderID: questions.recordset[i].LibraryFolderID,
+                        }
+                    }
+                }
+                question.Solution = Solution;
+                listQuestions.push(question);
+            }
+            data.Questions = listQuestions;
+            res.json({status: status.Access, data: data});
+        });
+    });
 });
 
 module.exports = router;
