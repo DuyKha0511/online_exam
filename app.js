17907d429ec206b4d0a079817b33822da513f0b6
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2021-12-20 19:06:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const gateway = require('express-gateway');
+// const gateway = require('express-gateway');
 const express = require('express'),
   app = express(),
   bodyParser = require("body-parser"),
